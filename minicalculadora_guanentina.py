ace932aeadd568f6a42159b68cf34ae54dfe3c75
Josix09 <jorgesilva@colegioguanenta.edu.co>
2025-02-06 17:49:59 -0500

    archivos actualizados

--- minicalculadora_guanentina.py
+++ minicalculadora_guanentina.py
@@ -1,26 +1,36 @@
 #input
+import math
+
 x=int(input("ingrese un número: "))
 
-y=int(input("ingrese otro número: "))
+y=int(input("ingrese un número: "))
 
 opc=int(input("ingrese una opc: "))
 
 #processing
 
 if opc==1:
-    print(x+y) #output
+   r=(x+y)
 
 if opc==2:
-    print(x-y) #output
+    r=(x-y) 
 
 if opc==3:
-    print(x*y) #output
+    r=(x*y) 
 
 if opc==4:
-    print(x/y) #output
+    r=(x/y) 
 
 if opc==5:    
-    print(x^y) #output
-
-
+    r=(x**y) 
     
+if opc==6:
+    
+    r = math.log (x,y) 
+
+else:
+    r=("opción no válida")
+
+#output
+
+print("resultado: " +str(r))
